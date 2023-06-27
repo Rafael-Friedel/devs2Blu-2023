6050040414e76f24a31db1dfe286ba1b03b575c6
Rafa Friedel <rafafriedeldev@outlook.com>
2023-06-27 16:24:19 -0300

    add attibutes to Pessoa class

--- exercicios/ravin/src/modelos/Pessoa.java
+++ exercicios/ravin/src/modelos/Pessoa.java
@@ -1,5 +1,9 @@
 package modelos;
 
 public class Pessoa {
-
+  private String nome;
+  private String telefone;
+  private Date dataNascimento;
+  private String cpf;
+  private String StatusFuncionario status;
 }
