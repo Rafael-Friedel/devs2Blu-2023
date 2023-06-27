89e54c37b188ec0a4d29866187507e08c30ca753
Rafael Friedel <rfriedel@furb.br>
2023-06-27 18:47:33 -0300

    add getters and setter in Produto class

--- exercicios/ravin/src/modelos/Produto.java
+++ exercicios/ravin/src/modelos/Produto.java
@@ -12,5 +12,60 @@ public class Produto {
   private StatusPreparo statusPreparo;
   private String tempoPreparo;
   private String observacoes;
-  private Status status; 
+  private Status status;
+  
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public Integer getCodigo() {
+	return codigo;
+}
+public void setCodigo(Integer codigo) {
+	this.codigo = codigo;
+}
+public Float getPrecoCusto() {
+	return precoCusto;
+}
+public void setPrecoCusto(Float precoCusto) {
+	this.precoCusto = precoCusto;
+}
+public Float getPrecoVenda() {
+	return precoVenda;
+}
+public void setPrecoVenda(Float precoVenda) {
+	this.precoVenda = precoVenda;
+}
+public StatusPreparo getStatusPreparo() {
+	return statusPreparo;
+}
+public void setStatusPreparo(StatusPreparo statusPreparo) {
+	this.statusPreparo = statusPreparo;
+}
+public String getTempoPreparo() {
+	return tempoPreparo;
+}
+public void setTempoPreparo(String tempoPreparo) {
+	this.tempoPreparo = tempoPreparo;
+}
+public String getObservacoes() {
+	return observacoes;
+}
+public void setObservacoes(String observacoes) {
+	this.observacoes = observacoes;
+}
+public Status getStatus() {
+	return status;
+}
+public void setStatus(Status status) {
+	this.status = status;
+} 
 }
