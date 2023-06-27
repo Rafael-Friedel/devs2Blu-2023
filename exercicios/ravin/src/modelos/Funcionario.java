3f7a773204ebdc5c01a0857b4dfa853055f1e2ee
Rafa Friedel <rafafriedeldev@outlook.com>
2023-06-27 16:25:01 -0300

    add attibutes to Funcionario class

--- exercicios/ravin/src/modelos/Funcionario.java
+++ exercicios/ravin/src/modelos/Funcionario.java
@@ -1,5 +1,12 @@
 package modelos;
 
-public class Funcionario {
-
+public class Funcionario extends Pessoa {
+  private String rg;
+  private String estadoCivil;
+  private String escolaridade;
+  private String cargo;
+  private String numeroCarteiraTrabalho;
+  private Date dataAdmissão;
+  private Date dataDemissão;
+  private String Disponibilidade disponibilidade;
 }
