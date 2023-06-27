97035b963750f9441ffd051fbb2e234795322ece
Rafa Friedel <rafafriedeldev@outlook.com>
2023-06-27 16:25:22 -0300

    add attibutes to Mesa class

--- exercicios/ravin/src/modelos/Mesa.java
+++ exercicios/ravin/src/modelos/Mesa.java
@@ -1,5 +1,11 @@
 package modelos;
 
 public class Mesa {
-
+  private Funcionario funcionario;
+  private String nome;
+  private Integer codigo;
+  private Integer numero;
+  private StatusMesa status;
+  private StatusMesa statusMesa;
+  private Integer quantidadeMaxPessoas;
 }
