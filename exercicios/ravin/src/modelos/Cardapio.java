52443f300b9282b46c60df0e9d989184e357aa30
Rafa Friedel <rafafriedeldev@outlook.com>
2023-06-27 16:26:00 -0300

    add attibutes to Cardapio class

---
+++ exercicios/ravin/src/modelos/Cardapio.java
@@ -0,0 +1,9 @@
+package modelos;
+import java.util.List;
+
+public class Cardapio {
+  private String nome;
+  private List<Produto> produtos;
+  private Integer codigo;
+  private Status status;
+}
