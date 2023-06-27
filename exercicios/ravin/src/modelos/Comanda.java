2dbfe413a5f42f7c0f77ff4f832183de6366b8f7
Rafa Friedel <rafafriedeldev@outlook.com>
2023-06-27 16:25:45 -0300

    add attibutes to Comanda class

--- exercicios/ravin/src/modelos/Comanda.java
+++ exercicios/ravin/src/modelos/Comanda.java
@@ -1,5 +1,11 @@
 package modelos;
+import java.util.List;
 
 public class Comanda {
-
+  private Mesa mesa;
+  private Cliente cliente;
+  private List<Produto> produtos;
+  private StatusComanda statusComanda;
+  private Integer codigo;
+  private Float valorTotalComanda;
 }
