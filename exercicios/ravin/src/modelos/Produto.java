4f5726bfc0cadabf632ecbf7fd4e912502c6bbbe
Rafa Friedel <rafafriedeldev@outlook.com>
2023-06-27 16:25:32 -0300

    add attibutes to Produto class

--- exercicios/ravin/src/modelos/Produto.java
+++ exercicios/ravin/src/modelos/Produto.java
@@ -1,5 +1,13 @@
 package modelos;
 
 public class Produto {
-
+  private String nome;
+  private String descricao;
+  private Integer codigo;
+  private Float precoCusto;
+  private Float precoVenda;
+  private StatusPreparo statusPreparo;
+  private String tempoPreparo;
+  private String observacoes;
+  private Status status; 
 }
