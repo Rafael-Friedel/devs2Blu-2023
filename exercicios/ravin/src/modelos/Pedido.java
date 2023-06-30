b05a48ce974ed6a17117f1a3a4777b411f79455a
Rafael Friedel <rfriedel@furb.br>
2023-06-30 18:57:15 -0300

    refactor Pedido class

--- exercicios/ravin/src/modelos/Pedido.java
+++ exercicios/ravin/src/modelos/Pedido.java
@@ -1,14 +1,30 @@
 package modelos;
 
 import java.sql.Timestamp;
-
 import enums.StatusPreparo;
 
 public class Pedido {
+	private int id;
+	private Produto produto;
 	private Timestamp dataHoraSolicitacao;
-	private String observacao;
-	private Integer quantidade;
+	private Timestamp dataHoraInicioPreparo;
+	private Timestamp tempoPreparoRestante;
 	private StatusPreparo statusPreparo;
+	private String observacao;
+	private int quantidade;
+	
+	public Pedido(int id, Produto produto, Timestamp dataHoraSolicitacao, Timestamp dataHoraInicioPreparo,
+			Timestamp tempoPreparoRestante, StatusPreparo statusPreparo, String observacao, int quantidade) {
+		super();
+		this.setId(id);
+		this.setProduto(produto);
+		this.dataHoraSolicitacao = dataHoraSolicitacao;
+		this.setDataHoraInicioPreparo(dataHoraInicioPreparo);
+		this.setTempoPreparoRestante(tempoPreparoRestante);
+		this.statusPreparo = statusPreparo;
+		this.observacao = observacao;
+		this.quantidade = quantidade;
+	}
 
 	public Timestamp getDataHoraSolicitacao() {
 		return dataHoraSolicitacao;
@@ -40,5 +56,37 @@ public class Pedido {
 
 	public void setStatusPreparo(StatusPreparo statusPreparo) {
 		this.statusPreparo = statusPreparo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Timestamp getDataHoraInicioPreparo() {
+		return dataHoraInicioPreparo;
+	}
+
+	public void setDataHoraInicioPreparo(Timestamp dataHoraInicioPreparo) {
+		this.dataHoraInicioPreparo = dataHoraInicioPreparo;
+	}
+
+	public Timestamp getTempoPreparoRestante() {
+		return tempoPreparoRestante;
+	}
+
+	public void setTempoPreparoRestante(Timestamp tempoPreparoRestante) {
+		this.tempoPreparoRestante = tempoPreparoRestante;
 	}
 }
